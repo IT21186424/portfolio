11a62135b91468ff69a4d186e31bd31041f96520
IT21186424 <99867312+IT21186424@users.noreply.github.com>
2025-03-24 16:00:22 +0530

    remove flash screen
    remove flash screen

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,15 +3,15 @@
 // To change portfolio colors globally go to the  _globalColor.scss file
 
 import emoji from "react-easy-emoji";
-import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+// import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
 
-// Splash Screen
+// // Splash Screen
 
-const splashScreen = {
-  enabled: true, // set false to disable splash screen
-  animation: splashAnimation,
-  duration: 2000 // Set animation duration as per your animation
-};
+// const splashScreen = {
+//   enabled: true, // set false to disable splash screen
+//   animation: splashAnimation,
+//   duration: 2000 // Set animation duration as per your animation
+// };
 
 // Summary And Greeting Section
 
