d12090ff5c0b876d0fd62d38dc1a94d3a0941099
IT21186424 <99867312+IT21186424@users.noreply.github.com>
2025-03-24 16:11:11 +0530

    Update portfolio.js
    update flash screen

--- src/portfolio.js
+++ src/portfolio.js
@@ -3,17 +3,17 @@
 // To change portfolio colors globally go to the  _globalColor.scss file
 
 import emoji from "react-easy-emoji";
-// import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
 
-// // Splash Screen
+// Splash Screen
 
-// const splashScreen = {
-//   enabled: true, // set false to disable splash screen
-//   animation: splashAnimation,
-//   duration: 2000 // Set animation duration as per your animation
-// };
+const splashScreen = {
+  enabled: false, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
 
-// Summary And Greeting Section
+//Summary And Greeting Section
 
 const illustration = {
   animated: true // Set to false to use static SVG
